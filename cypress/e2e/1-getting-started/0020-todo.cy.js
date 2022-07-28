575cc9bdb5512ffa201de908b3c309a987bfc788
Filipe Gabriel <komije4281@gmail.com>
2022-07-27 23:19:45 -0300

    ADD

---
+++ cypress/e2e/1-getting-started/0020-todo.cy.js
@@ -0,0 +1,220 @@
+/// <reference types="cypress" />
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
+
+describe('example to-do app', () => {
+  it('displays two todo items by default', () => {
+    cy.visit("https://www.youtube.com/watch?v=iaOpiSPbFNM&t")
+    cy.get('.yt-tooltip-renderer > .yt-simple-endpoint > #button').click()
+    cy.wait(4000)    
+   cy.get('.ytp-play-button').click()
+    cy.wait(4000)
+    cy.get('.ytp-play-button').click()
+  })
+})
